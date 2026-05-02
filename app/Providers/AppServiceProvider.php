8863674b95292d5790b4b3e5a9eb42398ff54ab0
Leonardo Fontes <lfontes@fca.uncu.edu.ar>
2026-05-02 14:51:07 +0000

    Remove legacy Livewire subpath route overrides

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Persona;
 use App\Models\Grupo;
@@ -26,7 +25,6 @@ use App\Observers\ParticipacionGrupoObserver;
 use App\Observers\WhatsAppMessageObserver;
 use App\Observers\AsistenciaObserver;
 use App\Observers\AsistenciaGrupoObserver;
-use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -47,8 +45,6 @@ class AppServiceProvider extends ServiceProvider
         Carbon::setLocale('es');
         Date::setLocale('es');
 
-        $this->configureLivewireRoutes();
-
         // Register model observers for activity logging
         Persona::observe(PersonaObserver::class);
         Grupo::observe(GrupoObserver::class);
@@ -60,24 +56,5 @@ class AppServiceProvider extends ServiceProvider
         WhatsAppMessage::observe(WhatsAppMessageObserver::class);
         Asistencia::observe(AsistenciaObserver::class);
         AsistenciaGrupo::observe(AsistenciaGrupoObserver::class);
-    }
-
-    protected function configureLivewireRoutes(): void
-    {
-        $path = trim((string) parse_url((string) config('app.url'), PHP_URL_PATH), '/');
-
-        if ($path === '') {
-            return;
-        }
-
-        config(['livewire.asset_url' => "/{$path}/livewire/livewire.js"]);
-
-        Livewire::setScriptRoute(function ($handle) use ($path) {
-            return Route::get("/{$path}/livewire/livewire.js", $handle);
-        });
-
-        Livewire::setUpdateRoute(function ($handle) use ($path) {
-            return Route::post("/{$path}/livewire/update", $handle);
-        });
     }
 }
